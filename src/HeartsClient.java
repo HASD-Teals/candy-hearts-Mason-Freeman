204ec3c87df5592d42a63b45fba21a2866f1dc13
Mason.Freeman0705 <Mefree05@yahoo.com>
2022-01-24 10:01:53 -0600

    InitialProperties

--- src/HeartsClient.java
+++ src/HeartsClient.java
@@ -1,5 +1,9 @@
+
 public class HeartsClient {
     public static void main(String[] args) throws Exception {
-        
+        Heart Test1 = new Heart();
+        Test1.setCost(50);
+        Test1.adjPrice(.25);
+        System.out.println(Test1.getCost());
     }
 }
